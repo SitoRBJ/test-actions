5caac6f810392831fdfaa16dbc190433a7ec92b7
SitoRBJ <sitoruizbravo@hotmail.com>
2022-12-01 13:42:43 +0100

    Udpate logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,13 @@ echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
 
-echo "VARS"
+echo "GITHUB_EVENT_NAME"
 echo $GITHUB_EVENT_NAME
+echo "GITHUB_REF_TYPE"
 echo $GITHUB_REF_TYPE
+echo "GITHUB_REF_NAME"
 echo $GITHUB_REF_NAME
+echo "GITHUB_BASE_REF"
 echo $GITHUB_BASE_REF
+echo "GITHUB_REF"
 echo $GITHUB_REF
