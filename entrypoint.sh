8c9cd239830f29a7ff7e083d644c93fd97f30fcb
SitoRBJ <sitoruizbravo@hotmail.com>
2022-12-01 16:13:08 +0100

    Test condition

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,3 +16,10 @@ echo "GITHUB_REF"
 echo $GITHUB_REF
 echo "GITHUB_HEAD_REF"
 echo $GITHUB_HEAD_REF
+
+
+if [[ ${GITHUB_EVENT_NAME} == *"pull_request"* ]]; then
+    echo "This is a PR"
+else
+    echo "This is not a PR"
+fi
