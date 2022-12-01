21be6080a085ef3b2dee63994e90469f9a357b3f
SitoRBJ <sitoruizbravo@hotmail.com>
2022-12-01 16:42:52 +0100

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ echo $GITHUB_HEAD_REF
 if [[ "pull_request" = ${GITHUB_EVENT_NAME} ]]; then
     echo "This is a PR"
     git diff ${GITHUB_BASE_REF} ${GITHUB_HEAD_REF} --dirstat
-    echot "THEN"
+    echo "git diff ${GITHUB_BASE_REF} ${GITHUB_HEAD_REF} --dirstat | awk -F '% ' '{print $2}'"
     TFSEC_DIRS=$(git diff ${GITHUB_BASE_REF} ${GITHUB_HEAD_REF} --dirstat | awk -F '% ' '{print $2}')
     echo $TFSEC_DIRS
 else
